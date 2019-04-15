1f79595c86bc4c79dceeafff440dc855064512c4
Nicholas Rabinowitz <nickr@uber.com>
2019-04-14 21:37:44 -0700

    Fix spreadsheet column headers in script

--- scripts/update-signers.js
+++ scripts/update-signers.js
@@ -1,6 +1,12 @@
 /* eslint-disable no-process-env, no-console */
 const GoogleSpreadsheet = require('google-spreadsheet');
 
+const SPREADSHEET_ID = '14dL-8nYBdlS0Ip055-HgPj81IvVLEGvzNZQwPpMZXVU';
+// We appear to be stuck with these column headers - they're set by the form.
+const NAME_COL =
+  'itheundersignedcurrentorformeruberemployeepledgetodonateatleast2ofthevalueofmyuberequitytocharity.';
+const JOB_COL = 'jobtitleoptional';
+
 const spreadsheetCredentialsPath = process.env.CREDENTIALS;
 if (!spreadsheetCredentialsPath) {
   throw new Error(
@@ -11,9 +17,7 @@ if (!spreadsheetCredentialsPath) {
 const creds = require(spreadsheetCredentialsPath);
 
 // Create a document object using the ID of the spreadsheet
-const doc = new GoogleSpreadsheet(
-  '14dL-8nYBdlS0Ip055-HgPj81IvVLEGvzNZQwPpMZXVU'
-);
+const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
 
 // Authenticate with the Google Spreadsheets API.
 doc.useServiceAccountAuth(creds, authErr => {
@@ -27,7 +31,7 @@ doc.useServiceAccountAuth(creds, authErr => {
     }
     const cleanRows = rows
       .filter(row => Boolean(row.ok))
-      .map(({name, jobtitle}) => ({name, jobtitle}));
+      .map(row => ({name: row[NAME_COL], jobtitle: row[JOB_COL]}));
     console.log(JSON.stringify(cleanRows, null, '  '));
   });
 });
