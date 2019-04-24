892a5f7538b183e67adab6b8446bf1e6ac852c00
Nicholas Rabinowitz <nickr@uber.com>
2019-04-24 14:16:33 -0700

    Add script to make the update-and-deploy flow a single command

---
+++ scripts/update-and-deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+yarn update-signers
+git diff
+
+echo -n "Diff looks good to go (y/n)? "
+read answer
+
+if [ "$answer" == "${answer#[Yy]}" ] ;then
+    exit 1
+fi
+
+git commit -am "Update signers" && git push
+yarn deploy
